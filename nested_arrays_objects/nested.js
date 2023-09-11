59986dabdba36a6b7b66c8b42804f52235c0f386
AlbertLeipzig <albertclemvill@gmail.com>
2023-09-11 09:07:23 +0200

    added nested array & objects folder·

---
+++ nested_arrays_objects/nested.js
@@ -0,0 +1,9 @@
+import fridgeData from './fridgeData.json' assert { type: 'json' };
+
+// manufacturer : X             ["samsung", "lg", "whirlpool"]
+// fridges color : X            ["stainless steel", "black", "white"]
+// has feature : "led display"
+// height mindestens 72
+// min temperature im fridge is at least 35
+// average consumption is max 150 watts
+// average rating is 3.5 or higher
